2164d9df4a0461b4cc150e3d8b396467be6ada24
Gonçalo Gama <72747189+Gama10Tech@users.noreply.github.com>
2021-12-22 10:57:22 +0000

    routes

--- movizz/src/main.js
+++ movizz/src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import {BootstrapVue} from "bootstrap-vue"
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css' 
+import 'bootstrap-vue/dist/bootstrap-vue.css' 
+import 'bootstrap/dist/js/bootstrap.js' 
+import 'bootstrap-vue/dist/bootstrap-vue.js' 
 
 new Vue({
   router,
--- movizz/src/router/index.js
+++ movizz/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Catalog from "../views/Catalog.vue";
+import Quizzes from "../views/Quizzes.vue";
+import Prizes from "../views/Prizes.vue";
+import Admin from "../views/Admin.vue";
+import Profile from "../views/Profile.vue";
+
 
 Vue.use(VueRouter);
 
@@ -11,13 +17,29 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/catalog",
+    name: "Catalog",
+    component: Catalog,
+  },
+  {
+    path: "/quizzes",
+    name: "Quizzes",
+    component: Quizzes,
+  },
+  {
+    path: "/prizes",
+    name: "Prizes",
+    component: Prizes,
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
   },
 ];
 
