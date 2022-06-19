d97a913f171e78f1529e97dba45649eebdc7a6c5
Darwin1337 <diogo.b.teixeira@hotmail.com>
2022-06-19 17:37:49 +0100

    update final

--- tests/fixCharacterName.test.js
+++
@@ -1,5 +0,0 @@
-const fixCharacterName = require('./fixCharacterName'); 
-test('Transformar o nome do personagem que vem da API', () => {
-    expect(fixCharacterName("Skyler White 62 episodes, 2008-2013")).toBe("Skyler White");
-});
-
